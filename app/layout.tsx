c1c72d013575990fa5fdef121e19f95debf65d6a
i9HUB Dev Team <dev@i9hubsst.com>
2025-11-10 17:13:57 -0300

    fix: Adiciona favicons explicitamente no head do HTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
